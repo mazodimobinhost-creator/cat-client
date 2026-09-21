868f63992b693ce58a9eff7048c390e551b3737e
mazodimobinhost-creator <m.azodi.mobinhost@gmail.com>
2026-09-21 05:23:19 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ android {
     defaultConfig {
         applicationId = "com.cat.client"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0.0"
 
